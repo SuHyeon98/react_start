77eeb699e1c7b1029100af6788008d041e810204
SuHyeon98 <114630366+SuHyeon98@users.noreply.github.com>
2024-08-16 21:35:19 +0900

    Add files via upload

---
+++ src/Yellowbox.tsx
@@ -0,0 +1,20 @@
+import { CSSProperties } from "react";
+
+const YellowBox = () => {
+  const boxType: CSSProperties = {
+    width: "200px",
+    height: "100px",
+    border: "2px solid black",
+    borderRadius: "10px",
+    margin: "0 auto",
+    backgroundColor: "yellow",
+    textAlign: "center",
+    alignItems: "center",
+  };
+
+  const msg = "집에 가고 싶음";
+
+  return <div style={boxType}>{msg}</div>;
+};
+
+export default YellowBox;
