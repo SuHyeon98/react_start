77cb05f09816e3bd4d42c78edc35b16ec8c990f9
SuHyeon98 <114630366+SuHyeon98@users.noreply.github.com>
2024-08-21 19:26:20 +0900

    Add files via upload

--- day03/src/App.tsx
+++ day03/src/App.tsx
@@ -1,34 +1,17 @@
 import React, { CSSProperties } from 'react';
 import Tiket, { TwayPriceProps } from './TwayTiket';
+import Button from './Button';
 
 // ctrl + w : 영역 선택
 // ctrl + alt + l : 라인정리
 // alt + j : 같은 단어 선택
 
 function App() {
-  const arr: TwayPriceProps[] = [
-    {
-      currency: 'KRW',
-      price: 50000,
-      seats: 2,
-    },
-    {
-      currency: 'KRW',
-      price: 40000,
-      seats: 3,
-    },
-    {
-      currency: 'KRW',
-      price: 70000,
-      seats: 0,
-    },
-  ];
-
   return (
-    <div style={{ display: 'flex' }}>
-      {arr.map((v) => (
-        <Tiket {...v} />
-      ))}
+    <div style={{ display: 'flex', gap: '10px' }}>
+      <Button backgroundColor="Primary" borderRadius="Hard" />
+      <Button backgroundColor="Primary" borderRadius="Smooth" />
+      <Button backgroundColor="Primary" borderRadius="Circle" />
     </div>
   );
 }
--- day03/src/Button.tsx
+++ day03/src/Button.tsx
@@ -4,29 +4,29 @@ type lineType = 'Hard' | 'Smooth' | 'Circle';
 type colorType = 'Primary' | 'Hover' | 'Secondary' | 'Deactive';
 
 type ButtonProps = {
-  borderRaius: lineType;
+  borderRadius: lineType;
   backgroundColor: colorType;
 };
 
-const Button = ({ backgroundColor, borderRaius }: ButtonProps) => {
-  const radius = {
-    Hard: '0px',
-    Smooth: '5px',
-    Circle: '43px',
-  };
+const radius = {
+  Hard: '0px',
+  Smooth: '5px',
+  Circle: '43px',
+};
 
-  const bg = {
-    Primary: '#205bf3',
-    Hover: '#478df5',
-    Secondary: 'transparents',
-    Deactive: '#979797',
-  };
+const bg = {
+  Primary: '#205bf3',
+  Hover: '#478df5',
+  Secondary: 'transparents',
+  Deactive: '#979797',
+};
 
+const Button = ({ backgroundColor, borderRadius }: ButtonProps) => {
   const ButtonStyle: CSSProperties = {
-    padding: '10px 45px',
     fontSize: '32px',
     color: 'white',
-    borderRadius: radius[borderRaius],
+    padding: '10px 45px',
+    borderRadius: radius[borderRadius],
     backgroundColor: bg[backgroundColor],
   };
 
